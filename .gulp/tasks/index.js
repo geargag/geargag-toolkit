90ed92e67d76c87fa9bc291e9f9a678376cf62cc
Hung Vu <vunamhung@gmail.com>
2019-11-22 10:49:49 +0700

    improve gulp tasks

--- .gulp/tasks/index.js
+++ .gulp/tasks/index.js
@@ -5,7 +5,14 @@ import { watchFiles } from "./watch";
 import { cleanDist, cleanDSStore, copyPlugin, deleteEmptyDir } from "./release";
 import { getPluginSize, replacePluginTexts, zipPlugin } from "./release";
 import { pushPlugins } from "./deploy";
+import { readmeToMarkdown } from "./general";
+import { buildPluginPotFile } from "./language";
 
 task("push:plugin", pushPlugins);
-task("release:plugin", series(cleanDist, cleanDSStore, copyPlugin, deleteEmptyDir, replacePluginTexts, zipPlugin, getPluginSize));
+task("build:lang", buildPluginPotFile);
+task("release:forGithub", series(cleanDist, cleanDSStore, copyPlugin, deleteEmptyDir, replacePluginTexts, zipPlugin));
+task(
+	"release:plugin",
+	series(cleanDist, cleanDSStore, copyPlugin, deleteEmptyDir, replacePluginTexts, zipPlugin, readmeToMarkdown, getPluginSize),
+);
 task("default", parallel(watchFiles, bsLocal));
