cdccb4bad337ee4d558a4fed463cc7c4d318e76b
Hung Vu <vunamhung@gmail.com>
2019-12-16 16:11:34 +0700

    update v2.1

--- src/plugin/index.php
+++ src/plugin/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: vnh_title
+ * Plugin Name: vnh_name
  * Description: vnh_description
  * Version: vnh_version
  * Tags: vnh_tags
@@ -10,10 +10,52 @@
  * License URI: vnh_license_uri
  * Document URI: vnh_document_uri
  * Text Domain: vnh_textdomain
+ * Tested up to: WordPress vnh_tested_up_to
+ * WC requires at least: vnh_wc_requires
+ * WC tested up to: vnh_wc_tested_up_to
  */
 
-define('vnh_plugin_file', __FILE__);
-define('vnh_plugin_dir', __DIR__);
+namespace GearGag_Toolkit;
 
-require __DIR__ . '/vendor/autoload.php';
-GearGag_Toolkit\Plugin::instance();
+use GearGag_Toolkit\tools\KSES;
+
+const PLUGIN_FILE = __FILE__;
+const PLUGIN_DIR = __DIR__;
+
+final class Plugin {
+	public function __construct() {
+		$this->load();
+		$this->init();
+		$this->core();
+		$this->boot();
+	}
+
+	public function load() {
+		require PLUGIN_DIR . '/vendor/autoload.php';
+	}
+
+	public function init() {
+		new KSES();
+	}
+
+	public function core() {
+		$this->add_db_column = new Add_DB_Column();
+		$this->add_db_column->boot();
+
+		$this->batch_delete = new Batch_Delete_Products();
+		$this->batch_delete->boot();
+
+		$this->woo = new Woo();
+		$this->woo->boot();
+	}
+
+	public function boot() {
+		add_action('plugin_loaded', [$this, 'load_plugin_textdomain']);
+	}
+
+	public function load_plugin_textdomain() {
+		load_plugin_textdomain('vnh_textdomain');
+	}
+}
+
+new Plugin();
